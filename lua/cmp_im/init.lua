fb50f485f4432cc56f3f198750dc777e4137655d
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2024-02-16 00:35:13 +0800

    first commit

---
+++ lua/cmp_im/init.lua
@@ -0,0 +1,194 @@
+local cmp = require("cmp")
+local utils = require("cmp_im.utils")
+local source = {}
+---Default options
+local im_opts = {
+	enable = true,
+	tables = utils.load_zhh_table(),
+	format = function(key, text)
+		return vim.fn.printf("%-15S %s", text, key)
+	end,
+	maxn = 8,
+}
+
+---All IM tables
+local im_tbls = nil
+
+-- 加载 table
+local function load_tbls(files)
+	-- 如果 opt 不启用，后面也就不加载码表
+	if not im_opts.enable then
+		return
+	end
+	if not im_tbls then
+		im_tbls = {}
+		for _, fn in ipairs(files) do
+			local tbl = utils.load_tbl(fn)
+			if tbl:valid() then
+				im_tbls[#im_tbls + 1] = tbl
+			else
+				vim.notify(string.format("Failed to load %s as cmp-im's table", fn), vim.log.levels.WARN)
+			end
+		end
+	end
+end
+
+local function cmp_item(key, val, params)
+	local ctx = params.context
+	local cur = ctx.cursor
+	return {
+		label = im_opts.format(key, val),
+		sortText = key,
+		filterText = key,
+		textEdit = {
+			newText = val,
+			insert = {
+				["start"] = { line = cur.line, character = cur.character - (cur.col - params.offset) },
+				["end"] = { line = cur.line, character = cur.character },
+			},
+		},
+	}
+end
+
+local function match_tbls(params)
+	local res = {}
+	if not im_tbls then
+		return res
+	end
+
+	local key = string.sub(params.context.cursor_before_line, params.offset)
+	for _, tbl in ipairs(im_tbls) do
+		local cnt = 0
+		if tbl:ordered() then
+			-- Match start from idx
+			local idx = tbl:index(key)
+			if idx then
+				repeat
+					local kvs = tbl.lst[idx + cnt]
+					if (not kvs) or (not string.match(kvs[1], "^" .. key)) then
+						break
+					end
+					for i, v in ipairs(kvs) do
+						if i >= 2 then
+							res[#res + 1] = cmp_item(kvs[1], v, params)
+							cnt = cnt + 1
+							if cnt >= im_opts.maxn then
+								break
+							end
+						end
+					end
+				until cnt >= im_opts.maxn
+			end
+		else
+			-- A brute force match that still provides a pretty acceptable performance!(Yes, luajit)
+			for _, kv in ipairs(tbl.lst) do
+				if string.match(kv[1], "^" .. key) then
+					cnt = cnt + 1
+					res[#res + 1] = cmp_item(kv[1], kv[2], params)
+				end
+				if cnt >= im_opts.maxn then
+					break
+				end
+			end
+		end
+	end
+
+	return res
+end
+
+function source:is_available()
+	return im_opts.enable
+end
+
+-- 综合起来，[[\l+]] 表示一个字符类，其中包含所有的字母字符和加号字符，即匹配任何一个字母字符或者加号字符。
+function source:get_keyword_pattern()
+	return [[\l\+]]
+end
+
+-- 触发 cmp 的按键
+function source:get_trigger_characters()
+	return {
+		"a",
+		"b",
+		"c",
+		"d",
+		"e",
+		"f",
+		"g",
+		"h",
+		"i",
+		"j",
+		"k",
+		"l",
+		"m",
+		"n",
+		"o",
+		"p",
+		"q",
+		"r",
+		"s",
+		"t",
+		"u",
+		"v",
+		"w",
+		"x",
+		"y",
+		"z",
+	}
+end
+
+function source:complete(params, callback)
+	load_tbls(im_opts.tables)
+	-- local t0 = vim.fn.reltime()
+	local res = match_tbls(params)
+	-- vim.notify('Match elapsed: ' .. tostring(vim.fn.reltimestr(vim.fn.reltime(t0))))
+	if #res > 0 then
+		return callback(res)
+	end
+	return callback()
+end
+
+---Setup IM's options
+local function setup(opts)
+	im_opts = vim.tbl_deep_extend("keep", opts, im_opts)
+end
+
+---Enable/Disable IM source
+local function toggle()
+	im_opts.enable = not im_opts.enable
+	return im_opts.enable
+end
+
+---Select the entry from IM
+local function select(index)
+	return function(fallback)
+		if im_opts.enable and cmp.visible() then
+			local entries = cmp.get_entries()
+			if index and index > 0 then
+				local num = 0
+				for k, e in ipairs(entries) do
+					if e.source.name == "IM" then
+						num = num + 1
+						if num >= index then
+							-- `count` works only after `select_next_item()` is called once at least
+							cmp.select_next_item()
+							cmp.select_next_item({ count = k - 1 })
+							return cmp.confirm()
+						end
+					end
+				end
+			end
+			if #entries > 0 and entries[1].source.name == "IM" then
+				return cmp.confirm({ select = true })
+			end
+		end
+		return fallback()
+	end
+end
+
+return {
+	source = source,
+	setup = setup,
+	toggle = toggle,
+	select = select,
+}
---
+++ lua/cmp_im/utils.lua
@@ -0,0 +1,113 @@
+local M = {}
+local T = {}
+
+function T.valid(self)
+	return #self.lst > 0
+end
+
+function T.ordered(self)
+	return self.inv
+end
+
+local function search(lst, key)
+	local lo = 1
+	local hi = #lst
+	while lo < hi do
+		local mi = math.floor((lo + hi) / 2)
+		if vim.stricmp(key, lst[mi][1]) > 0 then
+			lo = mi + 1 -- Make sure lst[lo] >= key
+		else
+			hi = mi
+		end
+	end
+	local idx = lo
+	if string.match(lst[idx][1], "^" .. key) then
+		return idx
+	end
+	return nil
+end
+
+---Search the IM-key within IM table list
+---Return lst index of IM-key that:
+---     lst[index - 1].key < lst[index].key
+--- and lst[index + 1].key >= lst[index].key
+--- and lst[index + 1].key =~# '^' .. lst[index].key
+function T.index(self, key)
+	local idx = self.inv[key]
+	if not idx then
+		idx = search(self.lst, key)
+	end
+	return idx
+end
+
+---String split with space by default
+local function split(line, sep)
+	if not sep then
+		sep = "%S+"
+	end
+	local list = {}
+	for ele in string.gmatch(line, sep) do
+		list[#list + 1] = ele
+	end
+	return list
+end
+
+function M.load_zhh_table()
+	local dir = vim.fs.dirname(vim.api.nvim_get_runtime_file("lua/cmp_im/init.lua", false)[1])
+	print(dir)
+	dir = vim.fs.dirname(vim.fs.dirname(dir))
+	local tbls = {}
+	tbls[#tbls + 1] = string.format("%s/tables/%s.txt", dir, "zhh")
+	print(vim.inspect(tbls))
+	return tbls
+end
+
+---Load IM table
+-- 这段Lua代码是一个函数，用于从文本文件中加载一个键值对表（key-values table）。让我解释一下它的工作原理：
+--
+-- M.load_tbl(filename) 函数接受一个文件名作为参数，该文件包含键值对数据。
+-- 函数首先尝试打开指定的文件 (io.open(filename, 'r'))。
+-- 如果成功打开文件，则创建了两个空表 lst 和 inv。lst 用于存储键值对列表，其中键为 lst[1]，其余为值。inv 用于存储键的反向索引，即键到位置的映射。
+-- 然后，函数逐行读取文件内容，对每一行进行处理。
+-- 如果某行包含至少两个部分（键和至少一个值），则将其分割，并将其添加到 lst 表中。然后，将该键添加到 inv 表中，如果该键之前没有出现过，则将其位置记录到 inv 表中。
+-- 此外，函数检查键的顺序是否按字母顺序排列。如果不是，则将 inv 表设置为 nil。
+-- 最后，函数返回一个包含 lst 和 inv 表的元表，其中 lst 表存储键值对列表，inv 表存储键的反向索引。
+-- 整体来说，该函数的作用是读取一个包含键值对数据的文本文件，并以表的形式返回这些数据。
+function M.load_tbl(filename)
+	-- 打开文件
+	local fp = io.open(filename, "r")
+	local lst = {} -- IM key-values list with key=lst[1] and values = lst[2:]
+	local inv = {} -- Inverted lst
+	local last = nil
+	local order = true
+
+	if fp then
+		local line = fp:read()
+		while line do
+			local parts = split(line)
+			if #parts >= 2 then
+				lst[#lst + 1] = parts
+
+				local key = parts[1]
+				if not inv[key] then
+					inv[key] = #lst
+				end
+
+				if order then
+					if last and vim.stricmp(last, key) > 0 then
+						order = false
+					end
+				end
+				last = key
+			end
+			line = fp:read()
+		end
+	end
+	if not order then
+		inv = nil
+	end
+
+	return setmetatable({ lst = lst, inv = inv }, { __index = T })
+end
+
+return M
